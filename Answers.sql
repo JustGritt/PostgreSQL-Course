13296abc44d7b83c050f6fd73bdc016b2d2c5b06
JustGritt <alexistan9712@gmail.com>
2022-11-22 17:21:42 +0100

    ✨ Add 3rd answer

--- Answers.sql
+++ Answers.sql
@@ -53,3 +53,41 @@ SELECT ADD_ALBUM('Futur ');
 SELECT ADD_ALBUM('Subliminal ');
 SELECT ADD_ALBUM('Futur ');
 SELECT ADD_ALBUM('fuTuR '); -- Same name but different cases
+
+-- ====================
+-- Question 3
+-- ====================
+CREATE OR REPLACE VIEW VIEW_ARTISTS AS
+SELECT artist.name, artist.birthdate, COUNT(song.id) AS songs
+FROM artist
+INNER JOIN song ON song.artist_id = artist.id
+GROUP BY artist.name, artist.birthdate
+ORDER BY artist.name ASC; 
+
+-- Execute the view
+SELECT * FROM VIEW_ARTISTS; -- You have to add a song to the artist first to see the result
+
+-- ====================
+-- Add a song to the database
+-- ====================
+CREATE OR REPLACE FUNCTION ADD_SONG(NAME VARCHAR(128), DURATION INTEGER, ARTIST_ID INTEGER)
+    RETURNS BOOLEAN AS $$
+BEGIN
+    PERFORM tmp.id FROM
+    ( 
+        SELECT song.id, song.name AS songName
+        FROM song
+    ) tmp 
+    WHERE tmp.songName = NAME;
+    IF FOUND THEN
+        RETURN FALSE;
+    ELSE
+        INSERT INTO song (name, duration, artist_id) VALUES (NAME, DURATION, ARTIST_ID);
+        RETURN TRUE;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Execute the function
+SELECT ADD_SONG('Less Than Zero', 199, 1); -- Artist ID 1 is The Weeknd
+-- Or use: INSERT INTO song (name, duration, artist_id) VALUES ('Less Than Zero', 199, 1);

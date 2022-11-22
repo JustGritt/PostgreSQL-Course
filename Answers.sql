d36c376c3883e67a68f4a484809fb43e9a535cd7
JustGritt <alexistan9712@gmail.com>
2022-11-22 18:25:48 +0100

    ✨ Add 5th answer

--- Answers.sql
+++ Answers.sql
@@ -24,6 +24,7 @@ SELECT ADD_ARTIST('The Weeknd', '1990-02-16');
 SELECT ADD_ARTIST('Black M', '1984-12-27');
 SELECT ADD_ARTIST('Sexion D''Assaut ', '2002-02-02');
 SELECT ADD_ARTIST('Black M', '2012-03-09');
+SELECT * FROM artist;
 
 -- ====================
 -- Question 2
@@ -53,6 +54,7 @@ SELECT ADD_ALBUM('Futur ');
 SELECT ADD_ALBUM('Subliminal ');
 SELECT ADD_ALBUM('Futur ');
 SELECT ADD_ALBUM('fuTuR '); -- Same name but different cases
+SELECT * from album;
 
 -- ====================
 -- Question 3
@@ -91,6 +93,7 @@ $$ LANGUAGE plpgsql;
 -- Execute the function or insert a song directly in the database
 SELECT ADD_SONG('Less Than Zero', 199, 1); -- Artist ID 1 is The Weeknd
 INSERT INTO song (name, duration, artist_id) VALUES ('Starry Eyes', 137, 1);
+SELECT * FROM song;
 
 -- ====================
 -- Question 4
@@ -107,3 +110,45 @@ ORDER BY album.name ASC;
 INSERT INTO album_song (album_id, song_id, track) VALUES (1, 1, 1); -- Album: Dawn FM, Song: Less Than Zero, track 1
 INSERT INTO album_song (album_id, song_id, track) VALUES (1, 2, 2); -- Album: Dawn FM, Song: Starry Eyes, track 2
 SELECT * FROM VIEW_ALBUMS;
+
+-- ====================
+-- Question 5 
+-- ====================
+CREATE OR REPLACE FUNCTION ADD_STOCK(ALBUM VARCHAR(128), AMOUNT INTEGER)
+    RETURNS BOOLEAN AS $$
+DECLARE 
+	tmpId integer;
+	stockValue integer;
+BEGIN
+    SELECT tmp.id into tmpId FROM
+    ( 
+        SELECT album.id, album.name AS albumNAme
+        FROM album
+    ) tmp 
+    WHERE tmp.albumNAme = ALBUM;
+	IF FOUND THEN
+		SELECT stock into stockValue FROM album_stock WHERE album_id = tmpId;
+		IF FOUND THEN
+			IF stockValue + AMOUNT >= 0 THEN
+				UPDATE album_stock SET stock = stockValue + AMOUNT WHERE album_id = tmpId;
+				RETURN TRUE;
+			ELSE
+				RETURN FALSE;
+			END IF;
+		ELSE
+			INSERT INTO album_stock (album_id, stock) VALUES (tmpId, AMOUNT);
+		END IF;
+		RETURN TRUE;
+	ELSE
+		RETURN FALSE;
+	END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Execute the function
+SELECT ADD_STOCK('Dawn FM', 10);
+SELECT ADD_STOCK('L''Ecole des points vitaux ', 5);
+SELECT ADD_STOCK('Unknown ', 3); -- Unknown doesn't exist in the database
+SELECT ADD_STOCK('Futur ', 1);
+SELECT * FROM album_stock;
+
